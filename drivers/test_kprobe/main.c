6a1f20c4ecdd7d94db6177c8585e13cdc0d950e6
Nam Cao <namcaov@gmail.com>
2023-08-21 16:08:20 +0200

    initial working kprobes test

--- drivers/test_kprobe/main.c
+++ drivers/test_kprobe/main.c
@@ -1,9 +1,10 @@
 #include <linux/module.h>
 #include <crypto/hash.h>
+#include <linux/slab.h>
+#include <linux/kprobes.h>
 MODULE_LICENSE("Dual BSD/GPL");
 
 #include "sha256.h"
-#include "sha256.c"
 
 const uint8_t firmware[] = "abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq";
 static const uint8_t reference_hash[32] = {
@@ -12,7 +13,7 @@ static const uint8_t reference_hash[32] = {
 	0xf6, 0xec, 0xed, 0xd4, 0x19, 0xdb, 0x06, 0xc1
 };
 
-static int test_sha256(void) {
+int test_sha256(void) {
 	struct tc_sha256_state_struct state;
 	uint8_t computed_hash[32];
 
@@ -28,12 +29,54 @@ static int test_sha256(void) {
 	}
 }
 
+static int callback_called = 0;
+static int kprobe_handler(struct kprobe *p, struct pt_regs *regs) {
+	printk("Handler called\n");
+	callback_called++;
+	return 0;
+}
+
+static int install_probes(void) {
+	struct kprobe *kp = kmalloc(5 * sizeof(struct kprobe), GFP_KERNEL);
+
+	int ret;
+	unsigned offset = 0;
+
+	for (int i = 0; i < 5; ++i) {
+		kp[i].symbol_name = "tc_sha256_final";
+		kp[i].pre_handler = kprobe_handler;
+		kp[i].offset = offset;
+
+		ret = register_kprobe(&kp[i]);
+		if (ret) {
+			printk("Couldn't register kprobe, err=%d\n", ret);
+			return -1;
+		} else {
+			printk("Successfully install  kprobe\n");
+		}
+
+		offset += GET_INSN_LENGTH(*kp[i].addr);
+	}
+
+	return 0;
+}
+
 static int __init module_start(void)
 {
+	printk("Hello world\n");
+
+	if (install_probes()) {
+		return -1;
+	}
+
 	if (test_sha256()) {
 		printk("Hashing failed\n");
 		return -1;
 	}
+
+	if (callback_called != 5)
+		printk("Expected number of callback to be 5, got %d\n", callback_called);
+
 	return 0;
 }
 
